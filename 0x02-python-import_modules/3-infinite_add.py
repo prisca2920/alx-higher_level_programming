ed5e474bd586169ad263f4c886f26c1c161062a9
prisca2920 <prisca2920@gmail.com>
2023-05-16 10:44:48 -0700

    adds args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from sys import argv
+    import sys
     suum = 0
-    for i in range(len(argv) - 1):
-        suum += int(argv[i + 1])
-        print('{}' .format(suum))
+    for i in range(len(sys.argv) - 1):
+        suum += int(sys.argv[i + 1])
+        print("{}" .format(suum))
