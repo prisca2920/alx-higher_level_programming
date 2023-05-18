2d28a052fac7556ec38c990c8ab944fcbd373177
prisca2920 <122819538+prisca2920@users.noreply.github.com>
2023-05-18 19:44:42 +0300

    Delete 0x17-doubly_linked_lists directory

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++
@@ -1,9 +0,0 @@
-#include "lists.h"
-
-/**
- * dlistint_len - returns the number of elements
- * @h: parameter to be checked
- * Return: no of elements
- */
-
-size_t dlistint_len(const dlistint_t *h)
