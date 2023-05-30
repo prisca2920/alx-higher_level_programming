027183183e31a69953db89508ade3117efbd2cfd
prisca2920 <prisca2920@gmail.com>
2023-05-29 19:42:34 -0700

    prints an int with an err message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    import sys
     try:
-        print("{:d}" .format(value))
-    except (ValueError, TypeError) as e:
-        sys.stderr.write("Exception: {}\n" .format(e))
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
     else:
         return True
