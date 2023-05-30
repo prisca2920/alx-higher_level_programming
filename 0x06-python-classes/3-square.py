d5760029a467ff5b1f241c33bd2a861c07d7e228
prisca2920 <prisca2920@gmail.com>
2023-05-30 11:13:00 -0700

    area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" defines area of a square """
+
+
+class Square:
+    """ defines area of a square """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
