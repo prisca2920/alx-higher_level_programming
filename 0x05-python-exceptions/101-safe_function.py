e0713609d2fe6d6a7b930af1bd7170c504245bc7
prisca2920 <prisca2920@gmail.com>
2023-05-29 19:51:31 -0700

    returns args

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
+    import sys
+    try:
+        i = fct(*args)
+        return i
+    except Exception as e:
+        sys.stderr.write("Exception: {}" .format(e))
+        return None
