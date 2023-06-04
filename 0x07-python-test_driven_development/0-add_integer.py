0606815f6573005b3ab6dec41b2c49923705f643
prisca2920 <prisca2920@gmail.com>
2023-06-04 09:30:33 -0700

    temp add

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" adds two integers"""
+
+
+def add_integer(a, b=98):
+    """ adds two integers """
+
+    result = 0
+    try:
+        a + b = result
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    else:
+        return result
