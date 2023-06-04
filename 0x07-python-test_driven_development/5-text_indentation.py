201f5846f0156b90560770be2f26af98356b8065
prisca2920 <prisca2920@gmail.com>
2023-06-04 10:35:54 -0700

    temp text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" prints a text with 2 new lines """
+
+
+def text_indentation(text):
+    """ prints a text with 2 new lines """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    else:
