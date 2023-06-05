9b805a26734d821e4b36f053c8383d7a625215b2
prisca2920 <prisca2920@gmail.com>
2023-06-05 00:40:20 -0700

    temp2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,13 +5,12 @@
 def add_integer(a, b=98):
     """ adds two integers """
 
-    result = 0
-    try:
-        a + b = result
     if type(a) is not int:
         raise TypeError("a must be an integer")
     if type(b) is not int:
         raise TypeError("b must be an integer")
 
     else:
+        result = 0
+        result = int(a) + int(b)
         return result
