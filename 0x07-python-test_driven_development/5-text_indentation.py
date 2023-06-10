593239ac471f2484e061b59868a77ff8c9d9c25e
prisca2920 <prisca2920@gmail.com>
2023-06-09 18:54:30 -0700

    text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,6 +5,11 @@
 def text_indentation(text):
     """ prints a text with 2 new lines """
 
+    chars = ['.', '?', ':']
     if type(text) is not str:
         raise TypeError("text must be a string")
     else:
+        for i in text:
+            print(i, end='')
+            if i in chars:
+                print('\n\n', end='')
