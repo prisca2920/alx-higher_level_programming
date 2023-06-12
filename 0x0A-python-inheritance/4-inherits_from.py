fca884261d77842df64f79302d12abfcc651f3ba
prisca2920 <prisca2920@gmail.com>
2023-06-12 10:57:30 -0700

    inherited

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,6 @@
 def inherits_from(obj, a_class):
     """inherited from a class """
 
-    return isinstance(obj, a_class)
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
