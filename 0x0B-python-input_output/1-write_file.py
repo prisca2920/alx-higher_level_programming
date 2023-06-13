70cd673ca8270e2cada00be4a70b83c5ee9a713b
prisca2920 <prisca2920@gmail.com>
2023-06-13 10:14:32 -0700

    write mode

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """writes to a file"""
     with open(filename, 'w', encoding='utf-8') as f:
-        texts = f.write(text)
-        print(texts)
+        return f.write(text)
