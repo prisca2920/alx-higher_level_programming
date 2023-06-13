89a052dfa870dad83fd88e5eae959f00a512ff4e
prisca2920 <prisca2920@gmail.com>
2023-06-12 19:43:29 -0700

    rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" inherits from a class"""
+
+
+class BaseGeometry:
+    """ inherits from a class"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ the new child class """
+    def __init__(self, width, height):
+        super().__init__
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}" .format(self.__width, self.__height)
