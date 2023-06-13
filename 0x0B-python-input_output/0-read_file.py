6de5ef5d6668722e571b1ccdb2f49a613cbb0537
prisca2920 <prisca2920@gmail.com>
2023-06-13 10:13:47 -0700

    read mode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """reads a text file """
     with open(filename, 'r', encoding='utf-8') as f:
-        f_content = f.read()
-        print(f_content)
+        print(f.read(), end="")
