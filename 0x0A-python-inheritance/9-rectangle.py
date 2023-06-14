cab36a029b45dde59a992f2fd5efe7d2c6305aa8
prisca2920 <prisca2920@gmail.com>
2023-06-14 04:04:32 -0700

    rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
 """ inherits from a class"""
-
-
-class BaseGeometry:
-    """ inherits from a class"""
-
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return (self.__width * self.__height)
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
