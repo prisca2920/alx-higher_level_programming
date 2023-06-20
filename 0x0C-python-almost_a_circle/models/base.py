0bff155295785bb2d42b06ed8c7809b022fbaef5
prisca2920 <prisca2920@gmail.com>
2023-06-20 10:31:58 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,49 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """saves the json rep to file"""
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w') as f:
+            if list_objs is None:
+                f.write('[]')
+            else:
+                the_dict = [i.to_dictionary() for i in list_objs]
+                f.write(Base.to_json_string(the_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of json str rep"""
+        if json_string is None or len(json_string) is 0:
+            return '[]'
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes set"""
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(5, 5)
+        elif cls.__name__ == 'Square':
+            dummy = cls(5)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = cls.__name__ + '.json'
+        instances = []
+        dicts = []
+
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                text = f.read()
+                dicts = cls.from_json_string(text)
+
+                for i in dicts:
+                    instances.append(cls.create(**dictionary))
+        return instances
