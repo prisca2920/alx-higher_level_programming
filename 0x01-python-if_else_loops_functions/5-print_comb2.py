aa64ae0f8716485ba6c9030ad3cdb1467b63c274
prisca2920 <prisca2920@gmail.com>
2023-10-04 09:21:45 -0700

    0-99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        print('{}{}'.format(i, j), end=', ')
-print()
+for i in range(0, 100):
+    print('{:02d}'.format(i), end=', ')
+print('99')
