26eb48109caf39bf0095274612f152cb382bc890
prisca2920 <prisca2920@gmail.com>
2023-10-05 18:39:35 -0700

    sys args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == '__main__':
+    args = len(sys.argv) - 1
+    if args == 1:
+        print('1 argument:')
+    elif args == 0:
+        print('0 argument.')
+    else:
+        print('{} arguments:'.format(args))
+
+    for i in range(args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
