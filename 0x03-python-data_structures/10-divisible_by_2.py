1f133a22481ec6e11a757c769657986f4f6a136b
prisca2920 <prisca2920@gmail.com>
2023-10-09 09:59:03 -0700

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     evens = []
-    for i in range(len(my_list) - 1):
+    for i in my_list:
         if i % 2 == 0:
             evens.append(True)
         else:
