6d6caf9909bb6a2d486d974351e50b4eb4e2ba33
prisca2920 <prisca2920@gmail.com>
2023-10-11 11:56:33 -0700

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     for key, value in a_dictionary.items():
-        a_dictionary.update(key, value)
-        print(a_dictionary)
+        a_dictionary[key] = value
+        return a_dictionary
     
