5da8e4e5d81c9aef96810c8eab76d06d6278fb24
prisca2920 <prisca2920@gmail.com>
2023-10-24 05:41:09 -0700

    private instance size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" defines a square by size"""
+
+class Square:
+    """ defines a square by size"""
+    def __init__(self, size):
+        """initializes the class square"""
+        self.__size = size
