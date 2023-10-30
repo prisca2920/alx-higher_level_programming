8b45bbe67adde9b34503354e9089ea1ef5bbb2ca
prisca2920 <prisca2920@gmail.com>
2023-10-30 07:49:00 -0700

    width height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" defining class rectangle"""
+
+
+class Rectangle:
+    """defining class rects"""
+    def __init__(self, width=0, height=0):
+        """initializing class rect"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """gets the width of a rect"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width of a rect"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """gets the height of a rect"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the height of a rect"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
