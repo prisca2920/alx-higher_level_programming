2fb57be112c4e7f3c46da3a0b7212c2fda1b1fb4
prisca2920 <prisca2920@gmail.com>
2023-11-06 05:09:21 -0800

    exact as type obj

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" if the object is exactly an instance"""
+
+
+def is_same_class(obj, a_class):
+    """ if the object is exactly an instance"""
+    if type(obj) == a_class:
+        return True
