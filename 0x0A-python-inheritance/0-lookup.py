44060be873adbbb19f60b60c0f2e5297ac0be4d7
prisca2920 <prisca2920@gmail.com>
2023-11-06 05:20:57 -0800

    test file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""returns a list of attr and methods"""
+
+def lookup(obj):
+    print(getattr())
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a class MyList that inherits from list"""
+
+
+class Mylist(list):
+    """inherits from list"""
+    def print_sorted(self):
+        return list.sort()
