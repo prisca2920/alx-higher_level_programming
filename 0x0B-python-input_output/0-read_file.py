de061874139a3d585d3e6c54816a3c23f2b6938d
prisca2920 <prisca2920@gmail.com>
2023-11-07 04:26:05 -0800

    reads from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a func that reads a text file"""
+
+
+def read_file(filename=""):
+    """a func that reads a text file"""
+    with open (filename, 'r') as f:
+         text = f.read()
+         print(text)
