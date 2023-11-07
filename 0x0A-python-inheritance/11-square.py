dc084e51501ada963198ba56bf16ef54751567b5
prisca2920 <prisca2920@gmail.com>
2023-11-06 18:12:02 -0800

    str rep of sq

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""creating a class square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """creating class sq inheriting from rect"""
+    def __init__(self, size):
+        """initializing size"""
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
