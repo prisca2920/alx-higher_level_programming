7193f1593c1672fe9b0fea9518f04e485a34044f
prisca2920 <prisca2920@gmail.com>
2023-11-08 09:14:08 -0800

    trial

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" creating a class student"""
+
+
+class Student:
+    """ creating a class student"""
+    def __init__(self, first_name, last_name, age):
+        """initializing the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dict rep"""
+        if (type(attrs) == list and
+                all(type(attr) == str for attr in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace all attr of the student"""
+        for i, j in json.items():
+            setattr(self, i, j)
+
