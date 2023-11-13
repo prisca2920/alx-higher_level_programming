fed80c8ff10035033455ac3e98f8b253d99c7d1d
prisca2920 <prisca2920@gmail.com>
2023-11-13 03:49:36 -0800

    temp

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -77,6 +77,26 @@ class test_rectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle()
 
+    def test_inf_height(self):
+        """infinity height"""
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(3, float('inf'))
+
+    def test_nan_height(self):
+        """height as nan"""
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(3, float('nan'))
+
+    def test_frozenset_height(self):
+        """height as a frozen set"""
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(1, frozenset({1, 2, 3, 1}))
+
+    def test_range_height(self):
+        """height as a range"""
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(3, range(5))
+
     def test_width_str(self):
         """width as a str"""
         with self.assertRaises(TypeError):
@@ -112,6 +132,26 @@ class test_rectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle(1)
 
+    def test_inf_width(self):
+        """infinity width"""
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(float('inf'), 6)
+
+    def test_nan_width(self):
+        """width as nan"""
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(float('nan'), 6)
+
+    def test_frozenset_width(self):
+        """width as a frozen set"""
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(frozenset({1, 2, 3, 1}), 6)
+
+    def test_range_width(self):
+        """width as a range"""
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(range(5), 6)
+
     def test_x_str(self):
         """x as a str"""
         with self.assertRaises(TypeError):
@@ -142,6 +182,26 @@ class test_rectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             rect = Rectangle(3, 6, 1.3)
 
+    def test_inf_x(self):
+        """infinity x"""
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(3, 6, float('inf'))
+
+    def test_nan_x(self):
+        """x as nan"""
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(3, 6, float('nan'))
+
+    def test_frozenset_x(self):
+        """x as a frozen set"""
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(3, 6, frozenset({1, 2, 3, 1}))
+
+    def test_range_x(self):
+        """x as a range"""
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(3, 6, range(5))
+
     def test_y_str(self):
         """y as a str"""
         with self.assertRaises(TypeError):
@@ -171,6 +231,26 @@ class test_rectangle(unittest.TestCase):
         """y as a float"""
         with self.assertRaises(TypeError):
             rect = Rectangle(3, 6, 5, 1.3)
+
+    def test_inf_y(self):
+        """infinity y"""
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(3, 6, 5, float('inf'))
+
+    def test_nan_y(self):
+        """y as nan"""
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(3, 6, 5, float('nan'))
+
+    def test_frozenset_y(self):
+        """y as a frozen set"""
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(3, 6, 5, frozenset({1, 2, 3, 1}))
+
+    def test_range_y(self):
+        """y as a range"""
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(3, 6, 5, range(5))
 
     def test_area(self):
         """area of a rect"""
