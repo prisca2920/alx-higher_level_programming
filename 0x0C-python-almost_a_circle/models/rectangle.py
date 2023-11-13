4662ce63e635e3dda5a5e97573c167659a3a9451
prisca2920 <prisca2920@gmail.com>
2023-11-13 09:13:05 -0800

    rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ class Rectangle(Base):
     """creating a class rect"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """initializing the class """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
