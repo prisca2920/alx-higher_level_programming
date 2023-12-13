daadff2d3de7cfbf7380aa88ee8446b16665529b
prisca2920 <prisca2920@gmail.com>
2023-12-13 08:39:35 -0800

    trial

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- creates a second table
 -- and adds multiple rows
-CREATE TABLE IF NOT EXISTS second_table(
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO second_table(id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES(
 	1, "John", 10,
 	2, "Alex", 3,
