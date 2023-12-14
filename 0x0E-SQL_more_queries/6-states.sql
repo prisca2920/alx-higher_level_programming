87446f4f9915c0415b703a72c1ab1b91603e4a4f
prisca2920 <prisca2920@gmail.com>
2023-12-14 04:56:57 -0800

    ans

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- creates a database
 -- and creates a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256),
 	PRIMARY KEY(id)
