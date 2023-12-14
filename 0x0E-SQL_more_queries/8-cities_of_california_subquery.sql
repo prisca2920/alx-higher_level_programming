b6a6a8ab6da675cc9685ff81a8d064a823669520
prisca2920 <prisca2920@gmail.com>
2023-12-14 05:06:11 -0800

    ans

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all the cities
 -- that can be found in db
-SELECT cities FROM hbtn_0d_usa.states
+SELECT hbtn_0d_usa.cities FROM hbtn_0d_usa.states
 WHERE name = California
 ORDER BY cities.id ASC;
