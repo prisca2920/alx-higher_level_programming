7076777cd859f38eeeb43f09eb56ca1b2aa15769
prisca2920 <prisca2920@gmail.com>
2023-12-14 04:36:38 -0800

    ans

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates a user
---and grant permission
+-- and grant permission
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
