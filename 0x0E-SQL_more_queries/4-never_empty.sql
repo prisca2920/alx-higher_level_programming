1e12423aeea10006011f1e531dbd8bb447279baf
prisca2920 <prisca2920@gmail.com>
2023-12-14 02:46:08 -0800

    ans

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- creates a table
+-- which id id never null
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+	);
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- creates a table
+-- which should be unique
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+	);
