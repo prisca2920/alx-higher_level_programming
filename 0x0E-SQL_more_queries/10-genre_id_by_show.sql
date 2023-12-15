09188b3ef610641c3907fd7565c6cf69fdf56dfd
prisca2920 <prisca2920@gmail.com>
2023-12-14 18:08:29 -0800

    ans

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- lists all the shows
 -- that have atleast one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows
+FROM tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title AND tv_show_genres.genre_id ASC;
