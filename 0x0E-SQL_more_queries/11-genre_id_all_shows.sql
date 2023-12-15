59137681c8ab6792cb19778fb3b49e926dd079a9
prisca2920 <prisca2920@gmail.com>
2023-12-14 18:15:01 -0800

    ans

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,7 @@
 -- lists all shows
 -- in the database
 SELECT tv_shows.title tv_show_genres.genre_id
-ORDER BY tv_shows.title AND tv_show_genres.genre_id ASC;
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
