2d0a0cd8c71d3a2fa23f386d78c7ab41393a16c6
prisca2920 <prisca2920@gmail.com>
2024-01-09 18:28:41 -0800

    draw square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const theSquare = require('./5-square');
+
+class Square extends theSquare {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
