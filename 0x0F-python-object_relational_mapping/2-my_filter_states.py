deb719feef0b81211a7243ccfc653e1f362de6c8
prisca2920 <prisca2920@gmail.com>
2024-01-12 05:18:28 -0800

    all values of states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 """displays all values in the states table """
 import sys
-import MYSQLdb
+import MySQLdb
 
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MYSQLdb.connect(
+    db = MySQLdb.connect(
             host='localhost',
             port=3306,
-            user=username,
-            passwd=password,
-            db=database
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
             )
 
     curs = db.cursor()
     curs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-            ORDER BY states.id ASC".format(argv[4]))
+            ORDER BY states.id ASC".format(sys.argv[4]))
 
     rows = curs.fetchall()
 
