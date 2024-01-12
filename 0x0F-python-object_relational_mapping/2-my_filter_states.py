46d9620d702264c2871076b65431acf098a43177
prisca2920 <prisca2920@gmail.com>
2024-01-12 05:04:40 -0800

    all values in states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,12 +5,16 @@ import MYSQLdb
 
 
 if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     db = MYSQLdb.connect(
             host='localhost',
             port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
+            user=username,
+            passwd=password,
+            db=database
             )
 
     curs = db.cursor()
