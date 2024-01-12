fe87ef67e9f4cbf45c2f5549e5979908b67003ff
prisca2920 <prisca2920@gmail.com>
2024-01-12 03:43:33 -0800

    letter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" lists all states with a name starting with N """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    connect = db.cursor()
+    connect.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+            ORDER BY states.id ASC")
+
+    rows = connect.fetchall()
+
+    fow row in rows:
+        print(row)
