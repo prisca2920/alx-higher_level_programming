a3be4e98157a7495de7d5e9aad65a0a66f2eb01b
prisca2920 <prisca2920@gmail.com>
2024-01-12 22:25:56 -0800

    update a state name

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""the first state obj in the db"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}"
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True
+    )
+
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
