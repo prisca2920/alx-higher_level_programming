e3e35ab8e494b38a2e81d84deee474f89e87ff91
prisca2920 <prisca2920@gmail.com>
2024-02-21 00:30:40 -0800

    no. of completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const URL = process.argv[2];
+const request = require('request');
+
+request(URL, (error, response, body) => {
+  if (!error) {
+    const data = JSON.parse(body);
+    const tasksByUser = {}; // Completed tasks
+    data.forEach(task => {
+      if (task.completed) {
+        if (tasksByUser[task.userId]) {
+          tasksByUser[task.userId]++;
+        } else {
+          tasksByUser[task.userId] = 1;
+        }
+      }
+    });
+    console.log(tasksByUser);
+  }
+});
