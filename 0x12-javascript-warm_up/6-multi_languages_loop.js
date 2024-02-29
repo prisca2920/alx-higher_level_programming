9a376b4b15756500510c29ed6f04b660cf642b38
prisca2920 <122819538+prisca2920@users.noreply.github.com>
2024-02-29 14:13:49 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const arg of args) {
-  console.log(arg);
-}
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const arg = parseInt(process.argv[2]);
-let i = 0;
-
-while (i < arg) {
-  console.log('C is fun');
-  i++;
-}
