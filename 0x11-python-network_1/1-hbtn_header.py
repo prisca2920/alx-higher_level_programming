50409222d1143586d2d3bce9779cbe5faf766d5c
prisca2920 <122819538+prisca2920@users.noreply.github.com>
2024-02-29 14:13:05 +0300

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""sends a request and displays value"""
-from sys import argv
-from urllib import request
-
-
-if __name__ == '__main__':
-    with request.urlopen(request.Request(argv[1])) as f:
-        print(dict(f.headers).get("X-Request-Id"))
