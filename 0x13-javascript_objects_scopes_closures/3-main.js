c026811b4bd9c48c9fc08846e48dd0af8e6ada5a
prisca2920 <prisca2920@gmail.com>
2024-03-12 01:12:13 -0700

    test file

---
+++ 0x13-javascript_objects_scopes_closures/3-main.js
@@ -0,0 +1,9 @@
+guillaume@ubuntu:~/0x13$ cat 3-main.js
+#!/usr/bin/node
+const Rectangle = require('./3-rectangle');
+
+const r1 = new Rectangle(2, 3);
+r1.print();
+
+const r2 = new Rectangle(10, 5);
+r2.print();
