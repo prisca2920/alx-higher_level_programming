15c44082b146319732f634e3ad355b00b02b46cf
prisca2920 <prisca2920@gmail.com>
2024-03-16 10:37:07 -0700

    queries as an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists all states with a name syarting with N"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """connects to mysql db"""
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+            ORDER BY states.id ASC".format(argv[4]))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
