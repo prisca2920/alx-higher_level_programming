15a9090c315164bf732bab4f09eb1752072ee26e
prisca2920 <prisca2920@gmail.com>
2024-03-16 11:14:40 -0700

    all cities by states

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lists cities by states"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """connects to mysql db"""
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name FROM cities JOIN states \
+            ON cities.state_id = states.id \
+            WHERE states.name LIKE BINARY %(state_name)s\
+            ORDER BY cities.id ASC", {'state_name': argv[4]})
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
