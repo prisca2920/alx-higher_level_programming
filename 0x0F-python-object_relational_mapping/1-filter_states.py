44c83269ce878ebbff68a08d0a9a7584df412ab5
prisca2920 <prisca2920@gmail.com>
2024-03-16 09:57:03 -0700

    capital N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     """connects to mysql db"""
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC')
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+            ORDER BY states.id ASC")
 
     rows = cur.fetchall()
 
