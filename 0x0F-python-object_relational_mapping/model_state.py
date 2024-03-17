29d95db77fa0ba08e50b96f3093a0fc09421b625
prisca2920 <prisca2920@gmail.com>
2024-03-16 21:40:25 -0700

    creating class state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""using sql alchemy orm """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class state that inherits from base"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
