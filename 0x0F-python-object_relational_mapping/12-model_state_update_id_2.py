d310af55d5ffdc349e9d562e694b12fa22113cd2
prisca2920 <prisca2920@gmail.com>
2024-03-17 18:44:53 -0700

    updates the name of a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""fetching all states"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+    """fetching all states via an orm"""
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+
+    state.name = 'New Mexico'
+
+    session.commit()
